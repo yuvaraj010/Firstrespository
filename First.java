ce0301320a09b91b46fc40d768cc34c4c7472692
yuvaraj010 <senthilyuvaraj40@gmail.com>
2021-12-08 12:12:53 +0530

    for loop update

--- First.java
+++ First.java
@@ -6,5 +6,8 @@ class First{
     public static void main(String[] args){
 
         System.out.println(" Not at all for mee ");
+        for(int i=0;i<10;i++){
+            System.out.println(" Number of "+i);
+        }
     }
 }
